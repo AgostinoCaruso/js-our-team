b33dfaf5579b88d72ead43d48c095266b203acd2
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-13 16:21:48 +0100

    print on DOM card

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@ import {teamMembers} from "./db.js"
 //add a form to add new members
 
 //div HTML
-const conainerCard = document.getElementById("container-card");
+const containerCard = document.getElementById("container-card");
 
-
+StartApp();
 
 function StartApp(){
     const fragment = document.createDocumentFragment();
@@ -23,24 +23,40 @@ function StartApp(){
     teamMembers.forEach(element => {
         //div
         const card = document.createElement("div");
-        card.className("card","col-4","d-flex");
+        card.className = "card col-4 d-flex justify-content-between flex-row align-items-center";
         
         const cardImg = document.createElement("div");
-        cardImg.className("card-img");
+        cardImg.className = "card-img";
         
         const cardText = document.createElement("div");
-        cardText.className("card-text");
+        cardText.className = "card-text";
         
         //ele in div
-        const img = document.createElement("img");
-        img.setAttribute("alt", "");
-        
         const name = document.createElement("p");
         const role = document.createElement("p");
         const email = document.createElement("p");
 
+        const img = document.createElement("img");
+        
         name.innerHTML = element.name;
+        role.innerHTML = element.role;
+        email.innerHTML = element.email;
+        img.src = element.img;
+
+        //add ele to HTML
+        cardText.appendChild(name);
+        cardText.appendChild(role);
+        cardText.appendChild(email);
+        
+        cardImg.appendChild(img);
+        
+        card.appendChild(cardImg);
+        card.appendChild(cardText);
+
+        fragment.appendChild(card);  
+        console.log({fragment});
     });
+    containerCard.appendChild(fragment);
 }
 
 // <div class="card col-12 col-md-4"></div>
