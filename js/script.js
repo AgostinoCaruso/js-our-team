f8fe796f4e93b5b07bc5dd973ede716e161916d0
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-13 18:38:56 +0100

    stable full screen card

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ Dato un array di oggetti rappresentante un team di un’azienda, creare una pagi
 - Aggiungere un form di aggiunta membri che permetta di visualizzare il nuovo membro sulla pagina (usate una foto qualunque, anche vostra se volete sentirvi parte del team! :sorridere:)
 */
 
-import {teamMembers} from "./db.js"
+import { teamMembers } from "./db.js"
 
 //todo
 //load all card and display
@@ -17,46 +17,89 @@ const containerCard = document.getElementById("container-card");
 
 StartApp();
 
-function StartApp(){
-    const fragment = document.createDocumentFragment();
-    
-    teamMembers.forEach(element => {
-        //div
-        const card = document.createElement("div");
-        card.className = "card col-4 d-flex justify-content-between flex-row align-items-center";
-        
-        const cardImg = document.createElement("div");
-        cardImg.className = "card-img";
-        
-        const cardText = document.createElement("div");
-        cardText.className = "card-text";
-        
-        //ele in div
-        const name = document.createElement("p");
-        const role = document.createElement("p");
-        const email = document.createElement("p");
+function StartApp() {
+    //const fragment = document.createDocumentFragment();
+    const template = teamMembers.map(element => `
+        <div class="col-4 p-2">
+            <div class= "myCard d-flex flex-row" >
+                <div class = "card-img">
+                    <img class="card-img-top" src="${element.img}" alt="${element.name}">
+                </div>
+                <div class="card-body card-text d-flex flex-column justify-content-start p-4">
+                    <h5 class="cardName">${element.name}</h5>
+                    <p class="cardRole">${element.role}</p>
+                    <a href="#" class="cardEmail">${element.email}</a>
 
-        const img = document.createElement("img");
-        
-        name.innerHTML = element.name;
-        role.innerHTML = element.role;
-        email.innerHTML = element.email;
-        img.src = element.img;
-
-        //add ele to HTML
-        cardText.appendChild(name);
-        cardText.appendChild(role);
-        cardText.appendChild(email);
-        
-        cardImg.appendChild(img);
-        
-        card.appendChild(cardImg);
-        card.appendChild(cardText);
-
-        fragment.appendChild(card);  
-        console.log({fragment});
-    });
-    containerCard.appendChild(fragment);
+                </div>
+            </div>
+        </div>
+    `).join("");
+    //containerCard.appendChild(fragment);
+    containerCard.innerHTML = template;
 }
 
-// <div class="card col-12 col-md-4"></div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//#region Element
+        // //div
+        // const card = document.createElement("div");
+        // card.className = "myCard col-4 d-flex justify-content-between flex-row align-items-center";
+
+        // const cardImg = document.createElement("div");
+        // cardImg.className = "card-img";
+
+        // const cardText = document.createElement("div");
+        // cardText.className = "card-text";
+
+        // //ele in div
+        // const name = document.createElement("p");
+        // const role = document.createElement("p");
+        // const email = document.createElement("p");
+
+        // const img = document.createElement("img");
+
+        // name.innerHTML = element.name;
+        // role.innerHTML = element.role;
+        // email.innerHTML = element.email;
+        // img.src = element.img;
+
+        // //add ele to HTML
+        // cardText.appendChild(name);
+        // cardText.appendChild(role);
+        // cardText.appendChild(email);
+
+        // cardImg.appendChild(img);
+
+        // card.appendChild(cardImg);
+        // card.appendChild(cardText);
+
+        // fragment.appendChild(card);  
+        // console.log({fragment});
+        //#endregion
